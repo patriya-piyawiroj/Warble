eaada3b4501e433bd0aaef7e53d7d163835c343b
patriya-piyawiroj <imnotateapot@gmail.com>
2020-01-28 19:08:51 -0800

    Linked commands to functions

--- a.cpp
+++ a.cpp
@@ -2,6 +2,9 @@
 #include <gflags/gflags.h>
 #include <iostream>
 
+#define INVALID_FLAG_ERROR "Invalid flag combinations"
+#define MISSING_ARGUMENT_ERROR "Missing arguments"
+
 DEFINE_string(registeruser, "", "Registers the given username");
 DEFINE_string(user, "", "Logs in as the given username");
 DEFINE_string(warble, "", "Creates a new warble with the given text");
@@ -10,18 +13,19 @@ DEFINE_string(follow, "", "Starts following the given username");
 DEFINE_string(read, "", "Reads the warble thread starting at the given id");
 DEFINE_bool(profile, false, "Get the user's profile of following and followers");
 
-// Success message
-
-// Error message
+// Prints error message
+void PrintError(char* errorMessage, char* fields) {
+	//printf("\nError: %s, %s", errorMessage, fields);
+}
 
 // Registers the given non-blank username
 void RegisterUser(std::string username) {
-	std::cout << username;
+	
 }
 
 // Posts a new warble (optionally as a reply), returns the id of new warble
 std::string Warble(std::string username, std::string text, std::string warbleID="") {
-	
+	return "";
 }
 
 // Starts following a given user
@@ -35,16 +39,29 @@ void Read(std::string warbleID) {
 }
 
 // Returns this user's following and followers
-void profile() {
-
-}
+void Profile() {
 
 }
 
 int main(int argc, char* argv[]) {
 	google::ParseCommandLineFlags(&argc, &argv, true);
+
 	if (FLAGS_registeruser != "") {
 		RegisterUser(FLAGS_registeruser);
+		return;
 	}
+
+	// if not user logged in, can not perform other functions
+	if (FLAGS_user == "") { 
+		//PrintError(MISSING_ARGUMENT_ERROR, "user");
+		return;
+	}
+
+	if (FLAGS_warble != "") {Warble(FLAGS_user, FLAGS_warble);} 
+	if (FLAGS_reply != "") {Warble(FLAGS_user, FLAGS_warble, FLAGS_reply);}
+	if (FLAGS_follow != "") {Follow(FLAGS_user, FLAGS_follow);}	
+	if (FLAGS_read != "") {Read(FLAGS_read);}
+	if (FLAGS_profile) {Profile();}
+
 	return 0;
 }
